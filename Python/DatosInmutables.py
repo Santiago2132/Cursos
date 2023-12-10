c3fc1a45617cb03befbd52c0c2a8bdcb19bad198
Santiago Maldonado Rojas <santiagomaldonadorojas64@gmail.com>
2023-12-09 22:50:39 -0500

    se agregaron cambios

---
+++ Python/DatosInmutables.py
@@ -0,0 +1,6 @@
+#Enteros son datos inmutables
+b = 5
+print(id(b))
+#Tipo texto es inmutable
+texto = "hola"
+print(texto)
---
+++ Python/flores.py
@@ -0,0 +1,28 @@
+import turtle
+import colorsys
+#Flores un detallito 
+def dibujar_flor(tortuga, radio, petalos):
+    for _ in range(petalos):
+        color = colorsys.hsv_to_rgb(_/petalos, 1.0, 1.0)
+        tortuga.pencolor(color)
+        dibujar_petalos(tortuga, radio)
+        tortuga.left(360/petalos)
+
+def dibujar_petalos(tortuga, radio):
+    heading = tortuga.heading()
+    tortuga.circle(radio, 60)
+    tortuga.left(120)
+    tortuga.circle(radio, 60)
+    tortuga.setheading(heading)
+
+mi_tortuga = turtle.Turtle()
+mi_tortuga.speed(0)  # Dibuja lo más rápido posible
+
+dibujar_flor(mi_tortuga, 100, 36)
+
+mi_tortuga.penup()
+mi_tortuga.goto(-50,-150)
+mi_tortuga.pendown()
+mi_tortuga.write("Te amo", font=("Arial", 16, "normal"))
+
+turtle.done()
